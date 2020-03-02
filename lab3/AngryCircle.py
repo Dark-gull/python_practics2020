bad2ccbe62958200586431f8c4650ade28f2bc36
Dark-gull <kantsurig@yandex.ru>
2020-03-02 15:25:29 +0300

    Master

---
+++ lab3/AngryCircle.py
@@ -0,0 +1,16 @@
+from graph import *
+"""Тестовое задание Злой Круг"""
+brushColor(255,255,0)
+circle(250,250,150)
+penColor(0,0,0)
+brushColor(255,0,0)
+circle(300,200,30)
+circle(175,200,25)
+brushColor(0,0,0)
+circle(175,200,10)
+circle(300,200,10)
+rectangle(165,290,315,310)
+polygon([(110,110),(180,200),(170,210),(120,125),(110,110)])
+run()
+
+
